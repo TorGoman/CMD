688cbbe1e6927f772dd4b30868340708e49f7082
TorGoman <amr200536@gmail.com>
2023-09-10 16:46:40 +0300

    Add files via upload

---
+++ Wget Download & Open - exe.cmd
@@ -0,0 +1,3 @@
+
+wget --no-check-certificate -q --show-progress -O "winrar-x64-623.exe" "https://www.win-rar.com/fileadmin/winrar-versions/winrar/winrar-x64-623.exe"
+start "" winrar-x64-623.exe /s
