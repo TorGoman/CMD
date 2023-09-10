1bb44df144a9bd65dd13f0d209bb242e19afae59
TorGoman <amr200536@gmail.com>
2023-09-10 16:55:07 +0300

    Update Wget Download & Open - exe.cmd

--- Wget Download & Open - exe.cmd
+++ Wget Download & Open - exe.cmd
@@ -1,3 +1,2 @@
-
 wget --no-check-certificate -q --show-progress -O "winrar-x64-623.exe" "https://www.win-rar.com/fileadmin/winrar-versions/winrar/winrar-x64-623.exe"
 start "" winrar-x64-623.exe /s
